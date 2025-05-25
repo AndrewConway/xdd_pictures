a934c153bfc246cb6da84a3434c2b76ce68dee37
Andrew Conway <arcgit@greatcactus.org>
2025-05-26 00:50:11 +1000

    Draw trionimoes on a chessboard

--- examples/draw_coverings.rs
+++ examples/draw_coverings.rs
@@ -3,21 +3,21 @@ use lattice_picture::item::LatticeItem;
 use lattice_picture::picture::LatticePicture;
 use lattice_picture::svg::{SizedSVG};
 use xdd::{BDDFactory, DecisionDiagramFactory, NoMultiplicity, VariableIndex};
-use chessboard_tiling_pictures::chessboard::setup_chessboard_tiled_with_dominoes;
+use chessboard_tiling_pictures::chessboard::{setup_chessboard_tiled_with_dominoes, setup_chessboard_tiled_with_up_to_trionimoes};
 use chessboard_tiling_pictures::tiling_problem::{SiteIndex, TilingProblem};
 
-fn count_tiling<F: DecisionDiagramFactory<u32, NoMultiplicity>>(problem:TilingProblem<[i32;2]>,to_draw:&[u128])  {
+fn count_tiling<F: DecisionDiagramFactory<u32, NoMultiplicity>>(problem:TilingProblem<[i32;2]>,to_draw:&[u128],name:&str)  {
     let (mut factory ,solution) = problem.find_tiling_solution::<F>();
     let original_len = factory.len();
     let renamer = factory.gc([solution]);
     let solution = renamer.rename(solution).unwrap();
-    let finder = factory.find_all_solutions::<u128>(solution,problem.tiles.len() as u16);
+    let finder = factory.find_all_solutions_with_minimal_true_arguments::<u128>(solution,problem.tiles.len() as u16);
     let gc_len = factory.len();
     println!("Original len {} gc len {} solutions {}",original_len,gc_len,finder.number_solutions());
     for i in to_draw {
         let sol = finder.get_ith_solution(*i).unwrap();
         let pic = draw_solution(&problem, &sol);
-        std::fs::write(format!("dominoes{}.svg",*i),format!("{}",pic.svg)).unwrap();
+        std::fs::write(format!("{name}{}.svg",*i),format!("{}",pic.svg)).unwrap();
         
     }
 }
@@ -41,5 +41,6 @@ fn draw_solution(problem:&TilingProblem<[i32;2]>,solution:&[VariableIndex]) -> S
 
 
 fn main() {
-    count_tiling::<BDDFactory<u32,NoMultiplicity>>(setup_chessboard_tiled_with_dominoes(8),&[0,1,5134723,5134724]);
+    count_tiling::<BDDFactory<u32,NoMultiplicity>>(setup_chessboard_tiled_with_dominoes(8),&[0,1,5134723,5134724],"dominoes"); // 0,1,5134723,5134724
+    count_tiling::<BDDFactory<u32,NoMultiplicity>>(setup_chessboard_tiled_with_up_to_trionimoes(8),&[0,1,173057372831,243057372831],"trionimoes");
 }
